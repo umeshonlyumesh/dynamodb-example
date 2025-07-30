7ac86b96796b982d3d5a3f46798ef81b68d39fe4
umeshonlyumesh <31796051+umeshonlyumesh@users.noreply.github.com>
2025-07-30 16:58:23 -0400

    Add files via upload

---
+++ DynamoDBConfig.java
@@ -0,0 +1,47 @@
+package com.dynamodb.dynamodbexample.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+@Configuration
+public class DynamoDBConfig {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.sessionToken}")
+    private String sessionToken;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public DynamoDbClient dynamoDbClient() {
+        AwsSessionCredentials credentials = AwsSessionCredentials.create(
+                accessKey,
+                secretKey,
+                sessionToken
+        );
+
+        return DynamoDbClient.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+    }
+
+    @Bean
+    public DynamoDbEnhancedClient dynamoDbEnhancedClient(DynamoDbClient dynamoDbClient) {
+        return DynamoDbEnhancedClient.builder()
+                .dynamoDbClient(dynamoDbClient)
+                .build();
+    }
+}
